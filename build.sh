828267e508fd1c64df100cb0d274b36ff8550f9b
Samuel Grayson <sam@samgrayson.me>
2020-03-04 20:05:52 -0600

    Install dependencies properly

--- build.sh
+++ build.sh
@@ -11,18 +11,12 @@ then
 	cd "${path}"
 fi
 
-if [ ! -e "/opt/glog" ]
-then
-	cd /opt
-	git clone https://github.com/google/glog.git
-	cd glog
-	./autogen.sh && ./configure && make && make install
-	cd "${path}"
-fi
+apt-get install cmake libgoogle-glog-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev libboost-dev libboost-filesystem-dev libopencv-dev libboost-filesystem-dev
 
 rm -rf build
 rm -rf install
 mkdir build && cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../install ..
-make -j8
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=../install -D CMAKE_C_COMPILER="$(which clang)" -D CMAKE_CXX_COMPILER="$(which clang++)" ..
+make -j$(nproc)
 make install
+cd ..
